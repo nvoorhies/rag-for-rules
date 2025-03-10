f7e8f3830092c0c3528c5320461e659ef2a06976
Nate Voorhies <nvoorhies@gmail.com>
2025-03-10 16:53:10 -0700

    Create eval_title_only.sh

---
+++ scripts/eval_title_only.sh
@@ -0,0 +1,6 @@
+python data/QA/evaluators/evaluate_qa.py \
+    --qa-pairs tmp/single_qa_3.json \
+    --srd tmp/srd.morefull.json \
+    --output tmp/title_path_references_scope_eval.json \
+    --system hierarchical \
+    --top-k 10
